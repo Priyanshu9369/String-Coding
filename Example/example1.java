a9187706bad98a3ad539d5991665638463f81fd9
Priyanshu Yadav <yadavpriyanshu9451@gmail.com>
2025-11-15 11:02:00 +0530

    Difference between string and string buffer and what happen in memory

---
+++ Example/example1.java
@@ -0,0 +1,14 @@
+package Example;
+// Differenece between String and Stringbuffer
+public class example1 {
+    public static void main(String[] args) {
+        String sc= new String("JAUNPUR");
+        String sc1=new String("JAUNPUR");
+        String SC1="JAUNPUR";
+        String sc2="JAUNPUR";
+        StringBuffer sc3=new StringBuffer("MARIAHU");
+        StringBuffer sc4=new StringBuffer("MARIAHU");
+        System.out.println(sc3);
+        System.out.println(sc4);
+    }
+}
